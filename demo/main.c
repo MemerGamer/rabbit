95a962c19c7549f04c44c226e2a05352dc9d547c
Nagy Szabolcs <szabi.nagy.ns@gmail.com>
2022-05-20 04:42:12 +0300

    added counter demo

--- demo/main.c
+++ demo/main.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include "../rabbit/headers/include.h"
 
-void my_function(RabbitArgArray* arg_array);
+void test(RabbitArgArray* arg_array);
+void count(RabbitArgArray* arg_array);
 
 int main() {
     rbt_init();
 
-    RabbitServer* server = rbt_create_server(AF_INET, IPPROTO_TCP, 80, RBT_HOST, 1);
+    RabbitServer* server = rbt_create_server(AF_INET, IPPROTO_TCP, 80, RBT_HOST, 20);
     rbt_start_server(server);
 
     rbt_add_static(&server, "/", "../demo/src/index.html");
     rbt_add_static(&server, "/test.txt", "../demo/src/test.txt");
     rbt_add_static(&server, "/Rabbit icon white.png", "../demo/src/Rabbit icon white.png");
     rbt_add_static(&server, "/background.png", "../demo/src/background.png");
-    rbt_add_api(&server, "/api/test", my_function, "%s %s %i %p", "alma", "korte", 1, server);
+    rbt_add_api(&server, "/api/test", test, "%s %s %i %p", "alma", "korte", 1, server);
+    rbt_add_static(&server, "/api/count", "../demo/src/count.txt");
+    rbt_add_api(&server, "/api/count/up", count);
 
     while(1){
         rbt_handle_request(&server);
     }
 }
 
-void my_function(RabbitArgArray* arg_array){
+void test(RabbitArgArray* arg_array){
     if (arg_array == NULL){
         printf("No arguments passed with function\n");
         return;
@@ -32,4 +35,12 @@ void my_function(RabbitArgArray* arg_array){
     printf("An integer: %i\n", arg_array->values[2]->integer);
     printf("A pointer: %p, which is a RabbitServer* pointer.\n", arg_array->values[3]->pointer);
     printf("And I can cast it, and get the address for example: %s\n", ((RabbitServer*)arg_array->values[3]->pointer)->address);
+}
+
+void count(RabbitArgArray* arg_array){
+    static int counter = 0;
+    freopen("../demo/src/count.txt", "w", stdout);
+    printf("%i", counter);
+    freopen("CON", "w", stdout);
+    counter++;
 }
